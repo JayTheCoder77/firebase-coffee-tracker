d93bc9635c278652450e1ffbcce6e04eacab1df4
JayTheCoder77 <jayantiyer90@gmail.com>
2025-03-17 17:56:25 +0530

    17 march 2025

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Layout from "./components/Layout"
+import Hero from "./components/Hero"
+
+function App() {
+
+  const isAuthenticated = false
+
+  return (
+    <Layout>
+        <Hero/>
+    </Layout>
+  )
+}
+
+export default App
