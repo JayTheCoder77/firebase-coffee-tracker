196251007d962ffdab0328e30f6dbc68fc008dce
JayTheCoder77 <jayantiyer90@gmail.com>
2025-03-18 18:16:09 +0530

    18 march 2025 start 5:14:00

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,21 @@
 export default function Hero(){
-    
+    return(
+        <>
+            <h1>Coffee Tracking for Coffee <abbr title = "An enthusiast or a devotee">Fiends</abbr></h1>
+            <div className="benefits-list">
+                <h3 className="font-bolder">Try <span className="text-gradient">Caffiend</span>and start ...</h3>
+                <p>✅ Tracking every coffee</p>
+                <p>✅ Measuring Your Blood caffiene level</p>
+                <p>✅ Costing and quantifying your addiction</p>
+            </div>
+            <div className="card info-card">
+                <div>
+                    <i className="fa-solid fa-circle-info"></i>
+                    <h3>Did You Know ...?</h3>
+                </div>
+                    <h5>That caffiene&apos;s half-life is about 5 hours</h5>
+                    <p>This means that after 5 Hours, half the caffiene you consumed is still in your system , keeping you alert longer ! So if you drink a cup of coffee with 200mg of caffiene , 5 Hours later, you'll still have about 100mg of caffiene in your system</p>
+                </div>
+        </>
+    )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,18 @@
 export default function Layout(props){
     const {children} = props
-    const header = (<header></header>)
-    const footer = (<footer></footer>)
+    const header = (<header>
+        <div>
+            <h1 className="text-gradient">CAFFIEND</h1>
+            <p>FOR Coffee Insatiates</p>     
+        </div>
+        <button>
+            <p>Sign Up Free</p>
+            <i className="fa-solid fa-mug-hot"></i>
+        </button>
+    </header>)
+    const footer = (<footer>
+        <p><span className="text-gradient">Caffiend</span> was made by <a href="https://github.com/JayTheCoder77" target="_blank">JayJr.<br/></a> using the <a href="https://linkedin.com" target="_blank">FantaCss</a> design library.</p>
+    </footer>)
     return(
         <>
             {header}
