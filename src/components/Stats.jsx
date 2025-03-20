8c0fd5e54941773bd0f6aa7139959abbeb33a193
JayTheCoder77 <jayantiyer90@gmail.com>
2025-03-20 18:01:41 +0530

    20 march 2025

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,44 @@
+import { calculateCoffeeStats, calculateCurrentCaffeineLevel, coffeeConsumptionHistory , statusLevels} from "../utils"
+
+function StatCard(props){
+    const {lg, title, children} = props
+    return(
+        <div className={'card stat-card ' + (lg ? ' col-span-2' : '')}>
+            <h4>{title}</h4>
+            {children}
+        </div>
+    )
+}
+
 export default function Stats(){
+    const stats = {
+        daily_caffiene:240,
+        daily_cost : 6.8,
+        average_coffees : 2.3,
+        total_cost : 220,
+
+    }
+    const caffeineLevel =  calculateCurrentCaffeineLevel(coffeeConsumptionHistory)
     return(
         <>
-        
+         <div className="section-header">
+            <i className="fa-solid fa-chart-simple" />
+            <h2>Stats</h2>
+         </div>
+         <div className="stats-grid">
+            <StatCard lg title = "Active Caffiene Level">
+                <div className="status">
+                    <p><span className="stat-text">{caffeineLevel}</span>mg</p>
+                    <p>
+                        <h5 style={{color : statusLevels['low'].color , background : statusLevels['low'].background}}>Low</h5>
+                    </p>
+                </div>
+            </StatCard>
+            <StatCard title = "Daily Caffiene"></StatCard>
+            <StatCard title = "Avg # of coffees"></StatCard>
+            <StatCard title = "Daily Cost ($)"></StatCard>
+            <StatCard title = "Total Cost ($)"></StatCard>
+         </div>
         </>
     )
 }
