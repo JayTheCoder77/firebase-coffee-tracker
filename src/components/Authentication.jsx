f6ee609a25d9f04a5ec5e1867cd75eedff07bffe
JayTheCoder77 <jayantiyer90@gmail.com>
2025-03-22 15:06:19 +0530

    22 march 2025

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,7 +1,16 @@
 export default function Authentication(){
     return(
         <>
-        
+            <h2 className="sign-up-text">Sign Up / Log In</h2>
+            <p>Sign In To Account</p>
+            <input placeholder="Email" />
+            <input placeholder="******" type="password" />
+            <button><p>Submit</p></button>
+            <hr />
+            <div className="register-content">
+                <p>Don&apos;t have an account?</p>
+                <button><p>Sign Up</p></button>
+            </div>
         </>
     )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,30 @@
+import { useState } from "react"
+import Modal from "./Modal" 
+import Authentication from "./Authentication"
 export default function Layout(props){
     const {children} = props
+
+    const [showModal , setShowModal] = useState(false)
     const header = (<header>
         <div>
             <h1 className="text-gradient">CAFFIEND</h1>
             <p>FOR Coffee Insatiates</p>     
         </div>
-        <button>
+        <button onClick={() => {setShowModal(true)}}>
             <p>Sign Up Free</p>
             <i className="fa-solid fa-mug-hot"></i>
         </button>
     </header>)
     const footer = (<footer>
-        <p><span className="text-gradient">Caffiend</span> was made by <a href="https://github.com/JayTheCoder77" target="_blank">JayJr.<br/></a> using the <a href="https://linkedin.com" target="_blank">FantaCss</a> design library.</p>
+        <p><span className="text-gradient">Caffiend</span> was made by <a href="https://github.com/JayTheCoder77" target="_blank">JayJr.<br/></a> using the <a href="https://linkedin.com" target="_blank">FantaCss</a> design library.<br/>Check out the project on <a href="https://github.com/JayTheCoder77/firebase-coffee-tracker" target="_blank">Github</a></p>
     </footer>)
     return(
         <>
+        {showModal && (    
+            <Modal handleCloseModal = {() => {setShowModal(false)}}>
+                <Authentication />
+            </Modal>
+        )}
             {header}
             <main>
                 {children}
